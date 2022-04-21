f5141e1d108a6c204bc837fb646940e4d7d0e7c3
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-21 02:49:32 +0100

    remove unuse file and refactore the App function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
