8b1da912da3fd9b7c80f55857eb42f6aaf30ae1b
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-28 16:09:35 +0100

    create three components home welcome amd quote

---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => (
+  <p className="quote">
+    Mathematics is not about numbers, equations, computations, or algorithms: it is about
+    understanding. -
+    <span> William Paul Thurston</span>
+  </p>
+);
+
+export default Quote;
---
+++ src/components/Welcome.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Welcome = () => (
+  <div className="content">
+    <h2>Welcome to our page!</h2>
+    <div>
+      Lorem ipsum dolor sit amet. Eos voluptas cumque ea quaerat rerum est molestiae voluptatem. Sit
+      laudantium minima non eveniet molestiae vel sint aliquid. Sed magnam galisum nam ipsam dolorem
+      qui laborum quia et iusto necessitatibus id distinctio consectetur et fugit eligendi est autem
+      quis! Sit porro nesciunt est veritatis omnis est molestiae aliquid aut sint modi eos
+      necessitatibus omnis id cumque reprehenderit.
+      Et facere unde qui tenetur tenetur id accusamus illo sed necessitatibus autem sed eveniet
+      magnam. Eum eaque corporis 33 eligendi sint vel tenetur rerum ut veritatis deleniti eos quidem
+      voluptatibus. Et dignissimos voluptatem hic cupiditate maiores et totam ipsam cum officia
+      autem quo eius suscipit.
+    </div>
+
+    <div>
+      Lorem ipsum dolor sit amet. Eos voluptas cumque ea quaerat rerum est molestiae voluptatem. Sit
+      laudantium minima non eveniet molestiae vel sint aliquid. Sed magnam galisum nam ipsam dolorem
+      qui laborum quia et iusto necessitatibus id distinctio consectetur et fugit eligendi est autem
+      quis! Sit porro nesciunt est veritatis omnis est molestiae aliquid aut sint modi eos
+      necessitatibus omnis id cumque reprehenderit.
+      Et facere unde qui tenetur tenetur id accusamus illo sed necessitatibus autem sed eveniet
+      magnam. Eum eaque corporis 33 eligendi sint vel tenetur rerum ut veritatis deleniti eos quidem
+      voluptatibus.
+    </div>
+  </div>
+);
+
+export default Welcome;
